55a6689983083d1602d8af3ba4b0da066a139c1b
GianmarcoDandrea <gianm96@hotmail.it>
2023-10-25 23:50:17 +0200

    add: button to add item in list

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-const shoppingList = ["pane", "pasta", "olio", "sale", "pepe", "zucchero"];
-
-
-let i = 0;
+//CONSTANTI
+const userInputElem = document.getElementById("input-elem-list");
+const insertBtn = document.getElementById("sendButton");
+const shoppingList = [];
 let listItem = "";
-while(i < shoppingList.length) {
-    listItem += `
-                <li>${shoppingList[i]}</li>`;
-    console.log(listItem);
-    
-    i++;
-}
 
-document.getElementById("list").innerHTML = listItem;
+// BOTTONE PER INSERIRE I VALORI DI INPUT NELL'ARRAY
+insertBtn.addEventListener("click", function () {
+
+    const listItem = userInputElem.value;
+    console.log(listItem);
+
+    shoppingList.push(listItem);
+    console.log(shoppingList);
+    userInputElem.value = "";
+    
+    // INSERIRE GLI ELEMENTI DELL'ARRAI DENTRO UL 
+    let i = 0;
+    let itemList = "";
+    
+    while(i < shoppingList.length ) {
+        itemList += `
+                    <li>${shoppingList[i]}</li>`;
+    
+        console.log(itemList);
+        
+        i++;
+    }
+    
+    document.getElementById("list").innerHTML = itemList;
+});
+
+
+
